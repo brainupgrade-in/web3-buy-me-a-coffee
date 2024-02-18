6e71131f29d1b015407e2b47ab5c70eb1de817b8
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-02-18 09:21:48 +0000

    init

---
+++ BuyMeACoffee-contracts/scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+  buyMeACoffee.waitForDeployment();
+
+  console.log(`Contract deployed at ${buyMeACoffee.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
